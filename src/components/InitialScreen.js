7feda429649bfda248ea29bf60aac479e58a0ca0
bartolliniRx <bartolliniRx@gmail.com>
2025-04-02 15:52:14 +0200

    InitialScreen WIP

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import styles from "./InitialScreen.module.scss";
 
 function InitialScreen({ addedClass = "" }) {
+	const [minutes, setMinutes] = useState("");
+	const [seconds, setSeconds] = useState("");
+
+	const numberValidate = (e, prev, setter) => {
+		let number = e.target.value.replace(/\D/g, "");
+		if (number > 59) number = prev;
+		setter(number);
+	};
+
 	return (
 		<div className={`${styles.initialScreen} ${addedClass}`}>
 			<div className={styles.timeInputs}>
-				<input type="number" placeholder="mm" step="1" min="0" max="60" />:
-				<input type="number" placeholder="ss" step="1" min="0" max="60" />
+				<input
+					type="text"
+					autoCorrect="off"
+					autoComplete="off"
+					inputMode="numeric"
+					pattern="[0-9]*"
+					placeholder="mm"
+					value={minutes}
+					onChange={e => numberValidate(e, minutes, setMinutes)}
+				/>
+				:
+				<input
+					type="text"
+					autoCorrect="off"
+					autoComplete="off"
+					inputMode="numeric"
+					pattern="[0-9]*"
+					placeholder="ss"
+					value={seconds}
+					onChange={e => numberValidate(e, seconds, setSeconds)}
+				/>
 			</div>
+			<button>START </button>
 		</div>
 	);
 }
