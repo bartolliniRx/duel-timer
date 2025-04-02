09838b4509a86119cd34cb09c355f9ce5ba8c4a8
bartolliniRx <bartolliniRx@gmail.com>
2025-04-02 18:20:54 +0200

    Enhanced timer value inputs

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -2,39 +2,49 @@ import { useState } from "react";
 import styles from "./InitialScreen.module.scss";
 
 function InitialScreen({ addedClass = "" }) {
-	const [minutes, setMinutes] = useState("");
-	const [seconds, setSeconds] = useState("");
+	const [minutes, setMinutes] = useState("01");
+	const [seconds, setSeconds] = useState("00");
 
-	const numberValidate = (e, prev, setter) => {
-		let number = e.target.value.replace(/\D/g, "");
-		if (number > 59) number = prev;
+	const numberValidate = (e, prev) => {
+		let number = e.target.value.replace(/\D/g, ""); // check for letters
+		if (number > 59 || number.length > 2) number = prev; // format & value validation
+		return number;
+	};
+
+	const leading0 = (e, setter, action) => {
+		let number = e;
+		if (action) {
+			if (number.length === 2 && number[0] === "0") number = number[1];
+		} else {
+			number = number.length === 1 ? "0" + number : number;
+		}
 		setter(number);
+	};
+
+	const timerInput = (val, setter) => {
+		return (
+			<input
+				type="text"
+				autoCorrect="off"
+				autoComplete="off"
+				inputMode="numeric"
+				pattern="[0-9]*"
+				placeholder="--"
+				value={val}
+				onFocus={e => {
+					leading0(val, setter, 1); // remove leading 0
+					setTimeout(() => e.target.select(), 10); // wait for value to change
+				}}
+				onBlur={() => leading0(val, setter, 0)} // add leading 0
+				onChange={e => setter(numberValidate(e, minutes))}
+			/>
+		);
 	};
 
 	return (
 		<div className={`${styles.initialScreen} ${addedClass}`}>
 			<div className={styles.timeInputs}>
-				<input
-					type="text"
-					autoCorrect="off"
-					autoComplete="off"
-					inputMode="numeric"
-					pattern="[0-9]*"
-					placeholder="mm"
-					value={minutes}
-					onChange={e => numberValidate(e, minutes, setMinutes)}
-				/>
-				:
-				<input
-					type="text"
-					autoCorrect="off"
-					autoComplete="off"
-					inputMode="numeric"
-					pattern="[0-9]*"
-					placeholder="ss"
-					value={seconds}
-					onChange={e => numberValidate(e, seconds, setSeconds)}
-				/>
+				{timerInput(minutes, setMinutes)}:{timerInput(seconds, setSeconds)}
 			</div>
 			<button>START </button>
 		</div>
